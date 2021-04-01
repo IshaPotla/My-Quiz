21f6d0db701b5b715aa3213b139c73190e9b2023
IshaPotla <71564379+IshaPotla@users.noreply.github.com>
2021-03-31 20:54:22 -0500

    Add files via upload

---
+++ question.js
@@ -0,0 +1,20 @@
+class Question{
+    constructor(){
+    
+    }
+
+    display(){
+        var title = createElement('h2')
+        title.html("MyQuiz Game");
+        title.position(350, 0);
+        var question = createElement('h2')
+        question.html("Question:- What starts and ends with the letter 'E' , but has only one letter? ");
+        question.position(150, 80);
+        // this.option1.html("1: Everyone ");
+        // this.option1.position(150, 100);
+        // this.option2.html("2: Envelope" );
+        // this.option2.position(150, 120);
+
+        // this.imput1.position(150, 230);
+      }
+}
